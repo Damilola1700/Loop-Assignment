83447bae76d84766a86aa8cab0b7696beba0c0a9
loveth <lovethdamilola206@gmail.com>
2026-05-13 09:29:05 +0100

    loop assignment

--- loop-ass.js
+++ loop-ass.js
@@ -1,20 +1,20 @@
 let evenNumber = 0;
 const myArray = [10, 40, "Apple", 21, 42, 11, 56, 40, "Banana"];
 
-let sum = 0;
+
 for (let i = 0; i < myArray.length; i++) {
   
 
   if (isNaN(myArray[i])) {
-    sum += myArray[i];
+    continue;
   } else {
-    if (myArray[i] % 2 === 0) {
+    if (myArray[i] % 2 === 0 && myArray[i] > 20) {
       evenNumber += myArray[i];
-    } else (myArray[i] )
+    } 
   }
 }
-console.log(sum);
+
 console.log(evenNumber);
-console.log(`The sum of even number is ${evenNumber}`);
+console.log(`The sum of even number greater than 20 is ${evenNumber}`);
 
 
